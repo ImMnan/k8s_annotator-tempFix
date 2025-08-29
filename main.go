e416016748256cccaa5eb0b32e491fe116756e41
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-08-29 17:05:23 +0530

    logggig

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -16,17 +15,11 @@ func main() {
 	agentmd.ShipID = os.Getenv("SHIP_ID")
 	agentmd.HarbourID = os.Getenv("HARBOUR_ID")
 	agentmd.Ns = os.Getenv("NAMESPACE")
-	agentmd.Interval = os.Getenv("INTERVAL")
 
 	fmt.Printf("\n[%v][INFO] Provided:\n ShipID: %s\n, HarbourID: %s\n, Namespace: %s", time.Now().Format("2006-01-02 15:04:05"), agentmd.ShipID, agentmd.HarbourID, agentmd.Ns)
 	// Parse interval string to integer (minutes)
-	intervalMinutes, err := strconv.Atoi(agentmd.Interval)
-	if err != nil {
-		fmt.Printf("\n[%v][ERROR] Invalid INTERVAL value: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
-		os.Exit(1)
-	}
 
-	ticker := time.NewTicker(intervalMinutes * time.Minute)
+	ticker := time.NewTicker(2 * time.Minute)
 	defer ticker.Stop()
 	for range ticker.C {
 		podUpdateAnnotaion(&agentmd)
